f02994c832e1878d96f6d68a20cac6b40d4242e0
Jake Luecke <jrluecke95@gmail.com>
2021-03-01 16:56:33 -0500

    done for the day

--- app.js
+++ app.js
@@ -26,15 +26,85 @@ const todoList = [
   },
 ];
 
+let nextId = 5;
+
 // GET /api/todos
+app.get('/api/todos', (req, res) => {
+  res.json(todoList); //send back todoList arary as JSON
+})
 
 // GET /api/todos/:id
+app.get('/api/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const todo = todoList.find((currTodo) => {
+    if (currTodo.id === id) {
+      return true;
+    } else {
+      return false
+    }
+  });
+
+  if (!todo) {
+    res.status(404).json({
+      error: `Could not find an item with ${id} for id`
+    });
+  } else {
+    res.json(todo);
+  }
+})
 
 // POST /api/todos
+app.post('/api/todos', (req, res) => {
+  if (req.body.description) {
+    const newTodo = {
+      id: nextId++,
+      description: req.body.description
+    };
+    todoList.push(newTodo);
+    res.status(201);
+    res.send();
+  } else {
+    res.status(422);
+    res.json({
+      error: 'please add a description'
+    })
+
+  }
+
+})
 
 // PUT /api/todos/:id
+app.patch('/api/todos/:id', (req, res) => {
+  if (req.body.description) {
+    const id = parseInt(req.params.id);
+    const todoIndex = todoList.findIndex((currTodo) => currTodo.id === id ? true : false);
+    todoList[todoIndex].description = req.body.description;
+
+    res.json(todoList[todoIndex])
+  } else {
+    res.status(422).json({
+      error: 'please provide a description'
+    })
+  }
+  
+
+})
 
 // DELETE /api/todos/:id
+app.delete('/api/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const todoIndex = todoList.findIndex((currTodo) => currTodo.id === id);
+  if (todoIndex !== -1) {
+    todoList.splice(todoIndex, 1);
+  } else {
+    res.status(404).json({
+      error: 'could not find item with that id'
+    })
+  }
+
+  res.status(204).json()
+  
+})
 
 app.listen(3000, function () {
   console.log('Todo List API is now listening on port 3000...');
